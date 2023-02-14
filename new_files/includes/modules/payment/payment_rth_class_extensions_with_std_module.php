53a9a4286f2598492ef2fd507e87e62a45927519
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-02-14 09:33:40 +0100

    chore: add methods for payment class

--- new_files/includes/modules/payment/payment_rth_class_extensions_with_std_module.php
+++ new_files/includes/modules/payment/payment_rth_class_extensions_with_std_module.php
@@ -24,5 +24,59 @@ class payment_rth_class_extensions_with_std_module extends StdModule
         $this->init('PAYMENT_RTH_CLASS_EXTENSIONS_WITH_STD_MODULE');
     }
 
+    public function update_status(): void
+    {
+    }
+
+    public function pre_confirmation_check(): void
+    {
+    }
+
+    public function selection(): array
+    {
+    }
+
+    public function confirmation(): array
+    {
+    }
+
+    public function process_button(): string
+    {
+    }
+
+    public function before_process(): void
+    {
+    }
+
+    public function payment_action(): void
+    {
+    }
+
+    public function before_send_order(): void
+    {
+    }
+
+    public function after_process(): void
+    {
+    }
+
+    public function success(): array
+    {
+    }
+
+    public function get_error(): array
+    {
+    }
+
+    public function iframeAction(): string
+    {
+    }
+
+    public function create_paypal_link()
+    {
+    }
+
+    public function info(): mixed
+    {
     }
 }

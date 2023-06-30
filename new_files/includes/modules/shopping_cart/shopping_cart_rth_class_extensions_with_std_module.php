2381e4ba2d67a7131c97887e03e01ffa28d6c8e9
Jay Trees <github.jay@grandel.anonaddy.me>
2023-06-30 12:17:25 +0200

    fix: remove static keyword (#2)

--- new_files/includes/modules/shopping_cart/shopping_cart_rth_class_extensions_with_std_module.php
+++ new_files/includes/modules/shopping_cart/shopping_cart_rth_class_extensions_with_std_module.php
@@ -35,107 +35,107 @@ class shopping_cart_rth_class_extensions_with_std_module extends StdModule
         parent::__construct($moduleType . '_' . 'SHOPPING_CART_RTH_CLASS_EXTENSIONS_WITH_STD_MODULE');
     }
 
-    public static function restore_contents_products_db($sqlDataArray, $productId, $tableBasket, $qty, $type)
+    public function restore_contents_products_db($sqlDataArray, $productId, $tableBasket, $qty, $type)
     {
         // Return ...
     }
 
-    public static function restore_contents_attributes_db($sqlDataArray, $productId, $value, $type)
+    public function restore_contents_attributes_db($sqlDataArray, $productId, $value, $type)
     {
         // Return ...
     }
 
-    public static function restore_contents_products_session($products, $tableBasket, $type)
+    public function restore_contents_products_session($products, $tableBasket, $type)
     {
         // Return ...
     }
 
-    public static function restore_contents_attributes_session($products, $tableBasketAttributes, $type)
+    public function restore_contents_attributes_session($products, $tableBasketAttributes, $type)
     {
         // Return ...
     }
 
-    public static function update_cart_products_session($productId, $type, $quantity, $attributes)
+    public function update_cart_products_session($productId, $type, $quantity, $attributes)
     {
         // Return ...
     }
 
-    public static function update_cart_products_db($sqlDataArray, $productId, $attributes, $type)
+    public function update_cart_products_db($sqlDataArray, $productId, $attributes, $type)
     {
         // Return ...
     }
 
-    public static function update_cart_attributes_session($value, $type, $productId, $option)
+    public function update_cart_attributes_session($value, $type, $productId, $option)
     {
         // Return ...
     }
 
-    public static function update_cart_attributes_db($sqlDataArray, $productId, $option, $type)
+    public function update_cart_attributes_db($sqlDataArray, $productId, $option, $type)
     {
         // Return ...
     }
 
-    public static function add_cart_products_session($productId, $type, $qty, $attributes)
+    public function add_cart_products_session($productId, $type, $qty, $attributes)
     {
         // Return ...
     }
 
-    public static function add_cart_products_db($sqlDataArray, $type)
+    public function add_cart_products_db($sqlDataArray, $type)
     {
         // Return ...
     }
 
-    public static function add_cart_attributes_session($value, $type, $productId, $option)
+    public function add_cart_attributes_session($value, $type, $productId, $option)
     {
         // Return ...
     }
 
-    public static function add_cart_attributes_db($sqlDataArray, $type)
+    public function add_cart_attributes_db($sqlDataArray, $type)
     {
         // Return ...
     }
 
-    public static function remove_custom_inputs_session($productId, $type)
+    public function remove_custom_inputs_session($productId, $type)
     {
         // Return ...
     }
 
-    public static function calculate_product_price($productPrice, $product, $contents, $productId)
+    public function calculate_product_price($productPrice, $product, $contents, $productId)
     {
         // Return ...
     }
 
-    public static function calculate_option_price($price, $option, $value, $productId, $qty)
+    public function calculate_option_price($price, $option, $value, $productId, $qty)
     {
         // Return ...
     }
 
-    public static function get_uprid($optValArray, $productId)
+    public function get_uprid($optValArray, $productId)
     {
         // Return ...
     }
 
-    public static function get_products($productsData, $product, $contents)
+    public function get_products($productsData, $product, $contents)
     {
         // Return ...
     }
 
-    public static function get_content_type($dbProductsId)
+    public function get_content_type($dbProductsId)
     {
         // Return ...
     }
 
-    public static function check_products_status_permission($status, $productId)
+    public function check_products_status_permission($status, $productId)
     {
         // Return ...
     }
 
-    public static function create_products_attributes_array($dataArray, $productId, $type)
+    public function create_products_attributes_array($dataArray, $productId, $type)
     {
         // Return ...
     }
 
-    public static function get_continue_shopping_link($url, $referer)
+    public function get_continue_shopping_link($url, $referer)
     {
         // Return ...
     }
